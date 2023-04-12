6f3bfa16c609aec57cd7ef30fcff481f9ea9fc8b
VietThang269 <114757489+VietThang269@users.noreply.github.com>
2023-04-12 21:02:09 +0700

    - detail order

--- src/pages/Orders.jsx
+++ src/pages/Orders.jsx
@@ -4,6 +4,8 @@ import {
   Button,
   createStyles,
   Flex,
+  Image,
+  Modal,
   Pagination,
   Select,
   Stack,
@@ -17,6 +19,10 @@ import {
   requestGetOrders,
   selectDataOrder,
 } from "../features/order/orderSlice";
+import IconDetail from "../assets/icons/IconDetail";
+import { apiGet } from "../utils/https/request";
+
+import Logo from "../assets/imgs/Logo.png";
 
 const Orders = () => {
   const { classes } = makeStyles();
@@ -75,6 +81,8 @@ const FilterOrder = () => {
 export const ListOrders = () => {
   const listData = useSelector(selectDataOrder);
   const [data, setData] = useState([]);
+  const [item, setItem] = useState(null);
+  const [opened, setOpened] = useState(false);
 
   useEffect(() => {
     if (listData.length > 0) {
@@ -82,12 +90,25 @@ export const ListOrders = () => {
         ...item,
         method: "COD",
         status: <Badge color="yellow">Pending</Badge>,
-        action: "",
+        action: (
+          <Box
+            style={{ cursor: "pointer" }}
+            onClick={() => handleDetail(item._id)}
+          >
+            <IconDetail />
+          </Box>
+        ),
       }));
 
       setData(newData);
     }
   }, [listData]);
+
+  function handleDetail(id) {
+    const newData = listData.find((item, _) => item._id === id);
+    setItem(newData);
+    setOpened(true);
+  }
 
   const ths = (
     <tr>
@@ -116,6 +137,130 @@ export const ListOrders = () => {
         <thead>{ths}</thead>
         <tbody>{rows}</tbody>
       </Table>
+      <ModalOrderDetail data={item} opened={opened} setOpened={setOpened} />
+    </Stack>
+  );
+};
+
+const ModalOrderDetail = ({ data, opened, setOpened }) => {
+  function calcTotal(data) {
+    if (data) {
+      const listTotal = data.list?.map((item, _) => item.price * item.quanity);
+      const total = listTotal.reduce((a, b) => a + b);
+
+      return total;
+    }
+    return 0;
+  }
+
+  return (
+    <Modal centered size={800} opened={opened} onClose={() => setOpened(false)}>
+      <Flex gap={20}>
+        <Stack
+          sx={{
+            flex: 1,
+          }}
+        >
+          <Text
+            align="center"
+            sx={{
+              fontWeight: "bold",
+              fontSize: 32,
+            }}
+          >
+            Order
+          </Text>
+          {data?.list.map((item, index) => (
+            <ItemOrderDetail data={item} key={index} />
+          ))}
+          <Text>
+            Total:{" "}
+            <Text display="inline" fw="bold">
+              ${calcTotal(data)}
+            </Text>
+          </Text>
+        </Stack>
+
+        <Stack
+          sx={{
+            flex: 1,
+          }}
+          align="center"
+        >
+          <Box>
+            <Image src={Logo} width={75} />
+          </Box>
+          <Stack w="100%" spacing={0}>
+            <Text fw="bold">
+              Customer name:{" "}
+              <Text display="inline" fw="normal">
+                Thắng nè
+              </Text>
+            </Text>
+          </Stack>
+          <Stack w="100%" spacing={0}>
+            <Text fw="bold">
+              Address:{" "}
+              <Text display="inline" fw="normal">
+                {data?.address}
+              </Text>
+            </Text>
+          </Stack>
+          <Stack w="100%" spacing={0}>
+            <Text fw="bold">
+              Status:{" "}
+              <Text display="inline" fw="normal">
+                Pending
+              </Text>
+            </Text>
+          </Stack>
+          <Button w="100%">Checkout</Button>
+        </Stack>
+      </Flex>
+    </Modal>
+  );
+};
+
+const ItemOrderDetail = ({ data }) => {
+  const [product, setProduct] = useState(null);
+  useEffect(() => {
+    const getDataProduct = async () => {
+      const response = await apiGet(`product/${data?.id}`, {});
+      setProduct(response.data);
+    };
+
+    getDataProduct();
+  }, [data]);
+
+  return (
+    <Stack
+      sx={{
+        borderBottom: "1px solid #ccc",
+      }}
+    >
+      <Flex gap={10}>
+        <Box
+          style={{
+            width: 75,
+            height: 75,
+          }}
+        >
+          <Image src={product?.image} />
+        </Box>
+
+        <Stack>
+          <Text>{product?.name}</Text>
+          <Text>
+            Quanity:{" "}
+            <Text display="inline" fw="bold">
+              {data?.quanity}
+            </Text>
+          </Text>
+        </Stack>
+      </Flex>
+      <Text align="end" fw="bold">
+        ${data?.quanity * data?.price}
+      </Text>
     </Stack>
   );
 };
